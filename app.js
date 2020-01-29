fd1352bed9cb32c60de60a27880fb7471d7029e1
nashawng-hyuna <nashawn.griffith@hyuna.bb>
2020-01-29 16:38:14 -0400

    update: managed to fix the 502 error with my upstream

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ const routes = require('./routes/routes');
 
 const port = process.env.PORT || 5000;
 
-// app.get('/api', (req, res) => {
-//   return res.send('Data Changed now');
-// });
-
 app.use('/api', routes);
 
 app.listen(port, () => {
-  console.log('running');
   console.log(`Listening on port ${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const name = process.env.NAME || 'World';
 
-router.get('/greet', (req, res) => {
-  return res.send(`Running from inside the router. Hello ${name}`);
+router.get('/get/', (req, res) => {
+  return res.json(`Running from inside the router. Hello ${name}`);
 });
 
 module.exports = router;
